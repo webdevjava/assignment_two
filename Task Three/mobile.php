ba0ff3b63c73301d929bd3615cf31243c9989ba8
webdevjava <hossain.m.rezwan@gmail.com>
2019-05-08 14:32:21 +0600

    Database

---
+++ Task Three/mobile.php
@@ -0,0 +1,14 @@
+<?php
+include 'connection.php';
+$connect = new connection();
+$connect->tenmobiles("Apple","Apple","100k");
+$connect->tenmobiles("Huawei","Mini","16000");
+$connect->tenmobiles("Apple","Apple","100k");
+$connect->tenmobiles("Apple","Apple","100k");
+$connect->tenmobiles("Nokia","Nokia Lumia","45000");
+$connect->tenmobiles("Apple","Apple","100k");
+$connect->tenmobiles("Apple","Apple","100k");
+$connect->tenmobiles("Apple","Apple","100k");
+$connect->tenmobiles("Apple","Apple","100k");
+$connect->tenmobiles("Nova","Nova100","24000");
+?>
--- mobile.php
+++
@@ -1,5 +0,0 @@
-<?php
-include 'connection.php';
-$connect = new connection();
-$connect->tenmobiles("Apple","Apple","100k");
-?>

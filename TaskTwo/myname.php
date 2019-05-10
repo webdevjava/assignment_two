d8d7123dc43b1345db9c35f4bf5db9fcabdd36a9
webdevjava <hossain.m.rezwan@gmail.com>
2019-05-11 04:46:35 +0600

    Added Files

--- TaskTwo/myname.php
+++ TaskTwo/myname.php
@@ -2,16 +2,7 @@
 include 'fullname.php';
 
 class myname extends fullname{
-    private $firstname;
-    private $secondname;
     
-    public function setData($firstname,$secondname){
-        $this->firstname=$firstname;
-        $this->secondname=$secondname;
-    }
-    public function getData(){
-        echo "My first name is ".$this->firstname."and my second name is ".$this->secondname;
-    }
     
      public function printname($name){
         echo "My nickname is ".$name;

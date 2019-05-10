62fcc2a5a74e81a26e7641e7ef323f4cd7e26854
webdevjava <hossain.m.rezwan@gmail.com>
2019-05-10 16:21:14 +0600

    Addes files

--- TaskTwo/fullname.php
+++ TaskTwo/fullname.php
@@ -1,7 +1,23 @@
 <?php
-class fullname implements interfacename{
-    private function printName(){
-        
+
+class fullname{
+    
+    private $firstname;
+    private $secondname;
+    
+    public function printname($name){
+        echo "My name is ".$name;
+    }
+    
+    
+    
+    public function setData($firstname,$secondname){
+        $this->firstname=$firstname;
+        $this->secondname=$secondname;
+    }
+    public function getData(){
+        echo "My first name is ".$this->firstname."and my second name is ".$this->secondname;
     }
 }
+
 ?>
--- TaskTwo/myname.php
+++ TaskTwo/myname.php
@@ -1,5 +1,20 @@
 <?php
-private class myname extends fullname{
+include 'fullname.php';
+
+class myname extends fullname{
+    private $firstname;
+    private $secondname;
     
+    public function setData($firstname,$secondname){
+        $this->firstname=$firstname;
+        $this->secondname=$secondname;
+    }
+    public function getData(){
+        echo "My first name is ".$this->firstname."and my second name is ".$this->secondname;
+    }
+    
+     public function printname($name){
+        echo "My nickname is ".$name;
+    }
 }
 ?>
